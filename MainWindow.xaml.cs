f8638eecb6762302052aca0481c37dd2a06cc790
Dane Balia <dane.balia@sourcing-sa.co.za>
2021-09-24 23:07:42 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DuckWalk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int tabCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newTabMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem tabItem = new TabItem();
+            wbTabControl.Items.Add(tabItem);
+            tabItem.Name = "tab_" + tabCount;
+            tabCount++;
+
+            tabItem.Header = $"New Blank Page ({tabCount})";
+
+        }
+
+        private void TabItem_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bkForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (defaultBrowser.CanGoForward)
+                defaultBrowser.ForwardCommand.Execute(sender);
+        }
+
+        private void bkBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (defaultBrowser.CanGoBack)
+                defaultBrowser.BackCommand.Execute(sender);
+        }
+
+        private void bkRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            defaultBrowser.ReloadCommand.Execute(sender);
+        }
+    }
+}
